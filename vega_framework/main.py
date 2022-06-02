0a16a76f480153c171a39464c222938401998e8a
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2022-06-02 23:50:59 +0300

    Lesson 2 (#2)
    * add: get, post requests
    
    * add: registration
    
    Co-authored-by: mmsolovev <mmsolovevmail@gmail.com>

--- vega_framework/main.py
+++ vega_framework/main.py
@@ -1,3 +1,7 @@
+from quopri import decodestring
+
+from vega_framework.requests import PostRequest, GetRequest
+
 
 def not_found_404_view(request):
     print(request)
@@ -10,17 +14,43 @@ class Vega:
         self.fronts = fronts
 
     def __call__(self, environ, start_response):
+
+        # path
         path = environ['PATH_INFO']
         if not path.endswith('/'):
             path = f'{path}/'
+
+        # request
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+        if method == 'POST':
+            data = PostRequest().get_request_params(environ)
+            request['data'] = Vega.decode_value(data)
+            print(f'Пришел POST-запрос: {Vega.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequest().get_request_params(environ)
+            request['request_params'] = Vega.decode_value(request_params)
+            print(f'Пришели GET-параметры: {Vega.decode_value(request_params)}')
+
+        # page controller
         if path in self.routes:
             view = self.routes[path]
         else:
             view = not_found_404_view
-        request = {}
+
         # front controller
         for front in self.fronts:
             front(request)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for key, value in data.items():
+            val = bytes(value.replace('%', '=').replace('+', ' '), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[key] = val_decode_str
+        return new_data
