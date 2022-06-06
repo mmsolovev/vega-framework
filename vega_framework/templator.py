5e79810c4560b7ebdfc62286315122f6569add4d
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2022-06-06 17:53:44 +0300

    add: templates refactor (#4)
    * add: templates refactor
    
    * fix: page look
    
    Co-authored-by: mmsolovev <mmsolovevmail@gmail.com>

--- vega_framework/templator.py
+++ vega_framework/templator.py
@@ -1,10 +1,10 @@
 from os.path import join
 
-from jinja2 import Template
+from jinja2 import Template, FileSystemLoader, Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
